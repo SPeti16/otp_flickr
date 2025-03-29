f5469f6c85ecb54947d2de6d265dfeb598f6423f
sarangpeter <sarang.peter@autosecurit.hu>
2025-03-29 19:09:39 +0100

    Delete the reason for warnings from Test

--- app/src/androidTest/java/com/test/otp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/test/otp/ExampleInstrumentedTest.kt
@@ -1,24 +1,13 @@
 package com.test.otp
 
-import android.widget.TextView
+
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
-import androidx.test.espresso.action.ViewActions.pressBack
-import androidx.test.espresso.action.ViewActions.typeText
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.isRoot
-import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import com.test.otp.network.FlickrApiService
-import com.test.otp.ui.FlickApp
 import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
@@ -31,16 +20,11 @@ import org.junit.Before
 import org.junit.Rule
 import retrofit2.Retrofit
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
+
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
-        // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.test.otp", appContext.packageName)
     }
